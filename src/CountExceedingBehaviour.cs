affef46452336a0dafe782f6e7e424640c6eaa41
Guiorgy <Guiorgy123@gmail.com>
2024-01-28 20:17:19 +0400

    fixed and optimized split methods

--- src/CountExceedingBehaviour.cs
+++ src/CountExceedingBehaviour.cs
@@ -1,17 +1,45 @@
-﻿namespace SpanExtensions
+﻿using System;
+
+namespace SpanExtensions
 {
     /// <summary>
-    /// Defines the behaviour of a split operation when there are more split instances than there may be. 
+    /// Defines the behaviour of a split operation when there are more split instances than there may be.
     /// </summary>
     public enum CountExceedingBehaviour
     {
         /// <summary>
         /// The last element returned will be all the remaining elements appended as one.
         /// </summary>
-        AppendRemainingElements,
+        AppendLastElements,
         /// <summary>
-        /// Every split instance more than permitted will not be returned. 
+        /// Every split instance more than permitted will not be returned.
         /// </summary>
-        CutRemainingElements
+        CutLastElements
+    }
+
+    /// <summary>
+    /// Extension methods for <see cref="CountExceedingBehaviour"/>.
+    /// </summary>
+    static class CountExceedingBehaviourExtensions
+    {
+        /// <summary>
+        /// Validates whether a specified value is a valid <see cref="CountExceedingBehaviour"/>.
+        /// </summary>
+        /// <param name="countExceedingBehaviour">The <see cref="CountExceedingBehaviour"/> to validate.</param>
+        /// <returns>The specified <see cref="CountExceedingBehaviour"/> if valid.</returns>
+        /// <exception cref="InvalidCountExceedingBehaviourException">If <paramref name="countExceedingBehaviour"/> is not valid.</exception>
+        public static CountExceedingBehaviour ThrowIfInvalid(this CountExceedingBehaviour countExceedingBehaviour)
+        {
+#if NET5_0_OR_GREATER
+            if(!Enum.IsDefined(countExceedingBehaviour))
+#else
+            if(!Enum.IsDefined(typeof(CountExceedingBehaviour), countExceedingBehaviour))
+#endif
+            {
+                throw new InvalidCountExceedingBehaviourException(countExceedingBehaviour);
+            }
+
+            return countExceedingBehaviour;
+        }
     }
 }
